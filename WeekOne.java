5bcca90c8a8fe89806ffa64fa96d7cd044dd0407
Abdul Atif <31568905+afifasoft@users.noreply.github.com>
2020-11-24 07:54:23 +0530

    Update WeekOne.java

--- WeekOne.java
+++ WeekOne.java
@@ -1,4 +1,22 @@
 // Implement in Java
+  
+// Q1
 // find the sum of all elements in an array 
 // int[] arr = { 20, 30, 40, 50, 10, 5};
 // output = 155
+
+
+// Q2
+// Reverse words in a given string
+// input: People also ask
+// output: ask also People
+
+// Q3
+// Reverse a string 
+// Input: Happy coding
+// Output: gnidoc yppaH
+
+// Q4
+// find the sum of two elements in an array
+// Input: int[] nums = {3, 2, 4, 6, 9} sum = 7
+// output = [0, 2] // Index of the elements
